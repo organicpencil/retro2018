98963f9ad58405305dac78bdfb4f1e2bfe91883b
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2019-03-02 13:09:32 -0600

    VehicleBody player controller

--- project/player/player.gd
+++ project/player/player.gd
@@ -1,1 +1,32 @@
-extends KinematicBody
+extends VehicleBody
+
+const STEER = 0.5
+const STEER_WEIGHT = 0.15
+const ANGLE_MAX = 20.0 # Degrees
+
+func _physics_process(delta):
+	var steer = 0.0
+	if Input.is_action_pressed("left"):
+		steer += 1.0
+	if Input.is_action_pressed("right"):
+		steer -= 1.0
+
+	var rot = rotation.y
+
+	if steer == 0.0:
+		steer = clamp(-rot, -STEER, STEER)
+
+	# Prevent going sideways/backwards
+	if rot > deg2rad(ANGLE_MAX):
+		steer = clamp(steer, -1, 0)
+	elif rot < -deg2rad(ANGLE_MAX):
+		steer = clamp(steer, 0, 1)
+
+	steering = lerp(steering, steer * STEER, STEER_WEIGHT)
+
+	engine_force = 200.0
+
+	# Vertical stabilizer
+	var local_rot = transform.basis.xform(rotation)
+	print(local_rot[2])
+	#add_torque(transform.basis.xform_inv(Vector3(0, 0, -local_rot[2] * 50.0)))
